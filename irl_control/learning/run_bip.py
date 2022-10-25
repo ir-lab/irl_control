4ac2f1fc08d66542c30c833ca694f202a2c1d1a2
Simon Stepputtis <7848799+sstepput@users.noreply.github.com>
2022-10-25 18:54:35 -0400

    Base Rotating Again

--- irl_control/learning/run_bip.py
+++ irl_control/learning/run_bip.py
@@ -14,6 +14,7 @@ from transforms3d.affines import compose
 from typing import Dict
 from control_base import ControlBase
 from BIP.bip_bimanual import IntprimStream
+from transforms3d.euler import quat2euler, euler2quat
 
 DEFAULT_EE_ROT = np.deg2rad([0, -90, -90])
 DEFAULT_EE_ORIENTATION = quat2euler(euler2quat(*DEFAULT_EE_ROT, 'sxyz'), 'rxyz')
@@ -46,6 +47,22 @@ class CollectData(ControlBase):
         action_config_name = 'iros2022_task.yaml'
         self.action_config = self.get_action_config(action_config_name)
         super().__init__(self.action_config["device_config"], robot_config_file, scene_file)
+
+    def send_bip(self, forces, gripper_force:float=None, update_errors:bool=True):      
+        # Apply forces to the main robot
+        for force_idx, force  in zip(*forces):
+            self.sim.data.ctrl[force_idx] = force
+        # Apply gripper force to the active arm
+        if gripper_force:
+            for idx in [7,14]:
+                self.sim.data.ctrl[idx] = gripper_force
+                
+        # Update the errors for every device
+        if update_errors:
+            for device_name in self.targets.keys():
+                # Calculate the euclidean norm between target and current to obtain error
+                self.errors[device_name] = np.linalg.norm(
+                    self.controller.calc_error(self.targets[device_name], self.robot.get_device(device_name)))
            
     def run(self, demo_type: str, demo_duration: int):        
         intprim = IntprimStream()
@@ -55,6 +72,11 @@ class CollectData(ControlBase):
         self.initialize_action_objects()
         self.run_sequence(self.action_config['iros2022_pickup_sequence'])
 
+        targets = {}
+        targets['base'] = Target()
+        targets['ur5left'] = Target()
+        targets['ur5right'] = Target()
+
         self.sim.step()
         step = 0
         while not intprim.is_done():
@@ -62,20 +84,16 @@ class CollectData(ControlBase):
             phase, prediction = intprim.update_stream(state)
             if phase is not None:
                 print("Phase at step {}: {:.2f}".format(step, phase))
-
+            
                 # Set Targets:
-                self.targets['ur5left'].setAllQuat(*np.take(prediction, [26,27,28, 32,33,34,35]))
-                self.targets['ur5right'].setAllQuat(*np.take(prediction, [29,30,31, 36,37,38,39]))
-                self.targets['base'].abg[2] = prediction[0]
+                targets['ur5left'].setAllQuat(*np.take(prediction, [26,27,28, 32,33,34,35]))
+                targets['ur5right'].setAllQuat(*np.take(prediction, [29,30,31, 36,37,38,39]))
+                targets['base'].abg[2] = prediction[0]
             
             # Generate an OSC signal to steer robot toward the targets
-            ctrlr_output = self.controller.generate(self.targets)
-            self.send_forces(ctrlr_output, gripper_force=self.gripper_force, update_errors=True)
+            ctrlr_output = self.controller.generate(targets)
+            self.send_bip(ctrlr_output, gripper_force=self.gripper_force, update_errors=True)
             
-            # Generate an OSC signal to steer robot toward the targets
-            # for force_idx, force  in zip(*ctrlr_output):
-            #     self.sim.data.ctrl[force_idx] = force
-
             # Step simulator / Render scene
             self.sim.step()
             self.viewer.render()   
