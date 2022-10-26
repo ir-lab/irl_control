6ab4bba26ce887a70147a64869e522c14f38df03
Michael Drolet <mdrolet01@gmail.com>
2022-10-26 02:35:32 +0000

    add comments

--- irl_control/device.py
+++ irl_control/device.py
@@ -62,6 +62,12 @@ class Device():
         actuator_trnids = model.actuator_trnid[:,0]
         self.ctrl_idxs = np.intersect1d(actuator_trnids, self.joint_ids_all, return_indices=True)[1]
         self.actuator_trnids = actuator_trnids[self.ctrl_idxs]
+        
+        # if self.name == "ur5right" or self.name == "ur5left":
+        #     self.sim.data.qpos[self.joint_ids[2:]] = np.copy(self.start_angles)
+        # elif self.name == "base":
+        #     self.sim.data.qpos[self.joint_ids[:1]] = np.copy(self.start_angles)
+        # self.sim.forward()
 
         # Check that the 
         if np.sum(np.hstack([self.ctrlr_dof_xyz, self.ctrlr_dof_abg])) > len(self.joint_ids):
